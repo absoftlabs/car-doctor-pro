e01552d028852fc40c45bd6c121a2fbd2070bedb
absoftlabs <abdullahtech2@outlook.com>
2025-04-05 01:25:52 +0600

    remove un neccessury padding form logo

--- src/components/Common/NavBar.jsx
+++ src/components/Common/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
                             }
                         </ul>
                     </div>
-                    <Link href='/' className="p-2">
+                    <Link href='/' className="py-2">
                         <Image src={logo} height={60} width={90} alt='logo' />
                     </Link>
                 </div>
