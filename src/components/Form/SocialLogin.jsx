e80d10747f09c5e7edb88814d011cc0eda6b9983
absoftlabs <abdullahtech2@outlook.com>
2025-04-12 11:48:56 +0600

    social login button customizing done

--- src/components/Form/SocialLogin.jsx
+++ src/components/Form/SocialLogin.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { IconBrandFacebookFilled, IconBrandGoogleFilled, IconBrandLinkedinFilled } from '@tabler/icons-react';
+import { IconBrandGithubFilled, IconBrandGoogleFilled } from '@tabler/icons-react';
 
 export default function SocialLogin() {
     return (
         <div className="flex gap-4 justify-center items-center">
-            <div className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center">
-                <IconBrandFacebookFilled className='text-blue-900' />
+            <div className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center hover:bg-info text-error hover:text-white active:scale-95 transition-all cursor-pointer">
+                <IconBrandGoogleFilled className='' />
             </div>
-            <div className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center">
-                <IconBrandLinkedinFilled className='text-blue-600' />
-            </div>
-            <div className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center">
-                <IconBrandGoogleFilled className='text-red-600' />
+            <div className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center hover:bg-info text-black hover:text-white active:scale-95 transition-all cursor-pointer">
+                <IconBrandGithubFilled className='' />
             </div>
         </div>
     )
