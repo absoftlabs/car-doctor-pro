ce91df927170311a7c98c330b1c33dfd333ce351
absoftlabs <abdullahtech2@outlook.com>
2025-04-12 18:20:21 +0600

    user redirection to home page after successfully loged in functionaly done

--- src/components/Form/SocialLogin.jsx
+++ src/components/Form/SocialLogin.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import { IconBrandGithubFilled, IconBrandGoogleFilled } from '@tabler/icons-react';
-import { signIn } from 'next-auth/react';
-import { redirect } from 'next/dist/server/api-utils';
+import { signIn, useSession } from 'next-auth/react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 export default function SocialLogin() {
-    const handleSocialLogin = async (providerName) => {
-        console.log("Social Login", providerName);
-        const result = await signIn(providerName, {redirect: false});
-        console.log(result);
-    }
+    const router = useRouter();
+    const session = useSession();
+
+    const handleSocialLogin = (providerName) => {
+        signIn(providerName);
+    };
+
+    useEffect(()=>{
+        if(session?.status == "authenticated"){
+            router.push('/');
+            toast.success('User loged in successfully')
+        }
+    },[session?.status])
     return (
         <div className="flex gap-4 justify-center items-center">
             <div onClick={() => handleSocialLogin('google')} className="bg-base-200 rounded-full h-[55px] w-[55px] grid place-content-center hover:bg-info text-error hover:text-white active:scale-95 transition-all cursor-pointer">
